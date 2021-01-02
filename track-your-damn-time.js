e416d5713ae0c1e82e75ccd12b60a9572ffd1fc6
patrykboch <42003319+patrykboch@users.noreply.github.com>
2021-01-02 23:11:52 +0100

    Update track-your-damn-time.js

--- track-your-damn-time.js
+++ track-your-damn-time.js
@@ -90,9 +90,11 @@ function checkDatesFrom(start, dataDir, done) {
         return checkDatesFrom(start.clone().add({ days: 1 }), dataDir, done);
     }
 
-    //Don't do future things
+    // Don't do future things
     if (start.isAfter(moment())) return done();
-//     //Do today if after 4pm
+    // Don't do previous things
+    if (start.isBefore(moment(), 'day')) return done();
+    //Do today if after 4pm
 //     if (start.isSame(moment(), 'day') && moment().hour() < 16) return done();
 
     checkAndPopulate(start, dataDir, function (err) {
